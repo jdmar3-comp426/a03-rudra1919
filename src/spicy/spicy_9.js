ed2f2ed5f15b363bb2f7eb6c337a7a61400e5595
rudra19@live.unc.edu <Rudy*1906$>
2021-11-28 15:36:43 -0500

    mb

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -47,7 +47,11 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-    
+    return function(num1){
+        return function(num2) {
+            return num1 * num2;
+        }
+    }
 };
 
 
