7f33e223c0dcee4b424f9375ab5b7f5c2d15dabd
rudra19@live.unc.edu <Rudy*1906$>
2021-11-28 16:24:44 -0500

    edit3

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -65,7 +65,7 @@ export const tenTimes = multiplyBy(10);
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(multiplyBy(50));
 };
 
 
