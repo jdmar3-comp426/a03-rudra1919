dfabdf0f363e367967dd681c2ade2ac10e8ee098
rudra19@live.unc.edu <Rudy*1906$>
2021-11-28 15:01:36 -0500

    spicy draft

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -15,7 +15,11 @@
  *                from calling the function
  */
 export const repeat = (fn, n, ...params) => {
-
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+       arr.push(fn(...params));
+    }
+    return arr;
 };
 
 
