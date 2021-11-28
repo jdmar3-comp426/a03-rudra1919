f83df071c09676b94f7965e4f4cda4df3dcb6697
rudra19@live.unc.edu <Rudy*1906$>
2021-11-28 10:37:18 -0500

    gm

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,7 +26,15 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-
+    array = array.sort(function (a,b) { return a - b; });
+    let odd = array.length % 2;
+    let half = Math.floor(array.length / 2)
+    if (odd == 1) {
+        return array[half -1] + array[half] / 2;
+    }
+    else {
+        return array[half];
+    }
 }
 
 /**
