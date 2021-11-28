0d92ae8380a8ce77838684ae30eb86a687926c62
rudra19@live.unc.edu <Rudy*1906$>
2021-11-28 09:59:31 -0500

    rknd

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -72,7 +72,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   let object2 = object;
+   let object2 =  {...object};
    delete object2[key];
    return object2;
 }
